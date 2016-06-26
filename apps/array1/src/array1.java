783a72a92e1ec7b9b5ae36332f5772ccac850d3d
VT <s147043656@gmail.com>
2016-06-26 19:15:12 +0300

    Array reverse order  and remove duplicates exercize

---
+++ apps/array1/src/array1.java
@@ -0,0 +1,55 @@
+public class array1 {
+	
+	public static void main(String[] args){
+
+		int[] names = {9, 10, 78, 10, 10, 10, 10, -4, 0, 100, 13, 45};
+		System.out.print("\nOriginal:\n");
+		for (int name: names) {
+			System.out.print(name + " ");
+			}
+
+		int[] names2 = reverseOrder(names);
+		System.out.print("\nReverse:\n");
+		for (int name: names2) {
+			System.out.print(name + " ");
+			}
+		
+		int[] names3 = removeDuplicates(names2);
+		System.out.print("\nWithout duplicates:\n");
+		for (int name: names3) {
+			System.out.print(name + " ");
+			}
+		}
+	
+	public static int[] reverseOrder( int[] array ) {
+		int[] sorted = new int[array.length];
+		int j = array.length - 1;
+		for (int i = 0; i < array.length; i++) {
+			sorted[j] = array[i];
+			j--;
+		}
+		return sorted;
+		
+	}
+	
+	public static int[] removeDuplicates( int[] array ) {
+	int end = array.length;
+	for (int i = 0; i < end; i++) {
+		for (int j = i + 1; j < end; j++) {
+			if (array[i] == array[j]) {
+				int shiftLeft = j;
+					for (int k = j+1; k < end; k++, shiftLeft++) {
+	                    array[shiftLeft] = array[k];
+	                }
+	                end--;
+	                j--;
+	            }
+	          }
+	        }
+	        int[] whitelist = new int[end];
+	        for(int i = 0; i < end; i++){
+	            whitelist[i] = array[i];
+	        }
+	        return whitelist;
+		}
+	}
